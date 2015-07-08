5f14171d67894230267d2ed640926ecdec8c99e2
Vadim Berezniker <vadim@berezniker.com>
2015-07-07 22:08:03 -0700

    Initial commit.

---
+++ poi_id.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import string
+
+import sys
+import pickle
+sys.path.append("../tools/")
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import test_classifier, dump_classifier_and_data
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+features_list = ['poi','salary'] # You will need to use more features
+
+### Load the dictionary containing the dataset
+data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
+
+def write_data_dict_to_csv(file):
+    all_features = data_dict["BAXTER JOHN C"].keys()
+    with open(file, "w") as f:
+        f.write("name," + string.join(all_features, ",") + "\n")
+        for person, person_data in data_dict.iteritems():
+            person_data_values = list(map(lambda feature: str(person_data[feature]), all_features))
+            f.write(person + "," + string.join(person_data_values, ",") + "\n")
+
+write_data_dict_to_csv("final_project_dataset.csv")
+
+### Task 2: Remove outliers
+### Task 3: Create new feature(s)
+### Store to my_dataset for easy export below.
+my_dataset = data_dict
+
+### Extract features and labels from dataset for local testing
+data = featureFormat(my_dataset, features_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+clf = GaussianNB()    # Provided to give you a starting point. Try a varity of classifiers.
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script.
+### Because of the small size of the dataset, the script uses stratified
+### shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+test_classifier(clf, my_dataset, features_list)
+
+### Dump your classifier, dataset, and features_list so 
+### anyone can run/check your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
